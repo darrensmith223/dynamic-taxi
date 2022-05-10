a303d4b700aef7c0e106cbe59f2f1ef86d6ee06c
Darren Smith <darren.smith@sparkpost.com>
2022-05-10 13:21:41 -0400

    Update countdown.php

--- app/countdown.php
+++ app/countdown.php
@@ -2,7 +2,6 @@
 $image = "https://www.sparkpost.com/wp-content/uploads/2016/07/SparkPost_Logo_Gray-Orange-1.png";
 $imageData = base64_encode(file_get_contents($image));
 header('Cache-Control: no-cache, max-age=0');
-$png_image = 'data:image/png;base64,'.$imageData;
-imagepng($png_image);
-imagedestroy($png_image);
+//echo '<img src="data:image/png;base64,'.$imageData.'">';
+imagepng($imageData);
 ?>
