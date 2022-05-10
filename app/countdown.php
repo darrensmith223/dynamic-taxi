d3283ddc41dc44bff2b968809447fe603d894495
Darren Smith <darren.smith@sparkpost.com>
2022-05-10 12:22:22 -0400

    Create countdown.php

---
+++ app/countdown.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>Countdown</title>
+ </head>
+ <body>
+ <?php echo '<p><img src="https://www.sparkpost.com/wp-content/uploads/2016/07/SparkPost_Logo_Gray-Orange-1.png"></p>';
+ ?> 
+ </body>
+</html>
